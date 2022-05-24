d64b9d6563d367ef65032f5cf2f0162a263767ca
Alaminswe <alamin.swe.bd@gmail.com>
2022-05-24 16:14:20 +0600

    first class

---
+++ Calculation.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main () {
+    int a = 10;
+    int b = 8;
+    int sum, c, d;
+
+    sum = a + b;
+    c = sum / 3;
+    d = c * 2;
+
+
+    printf("%d\n %d\n %d\n", sum, c, d);
+
+
+
+    return 0;
+}
---
+++ firstday.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main () {
+
+
+    printf("Its maybe sunny day,I saw your memory in every coding line,haha ,lack of tumi");
+
+
+
+    return 0;
+}
